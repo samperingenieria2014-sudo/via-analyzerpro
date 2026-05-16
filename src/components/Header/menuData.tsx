9754f111c41db852e854c3a2aaa9c2187a41fcb7
samperingenieria2014-sudo <samperingenieria2014@gmail.com>
2026-05-16 17:22:11 -0500

    fix menuData tipo Menu

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -1,30 +1,35 @@
 import { Menu } from "@/types/menu";
 
-const menuData = [
+const menuData: Menu[] = [
   {
     id: 1,
     title: "Inicio",
     path: "/",
+    newTab: false,
   },
   {
     id: 2,
     title: "Servicios",
     path: "#features",
+    newTab: false,
   },
   {
     id: 3,
     title: "Normativas",
     path: "#about",
+    newTab: false,
   },
   {
-  id: 4,
-  title: "Casos Reales",
-  path: "/#case-studies",
-},
+    id: 4,
+    title: "Casos Reales",
+    path: "/#case-studies",
+    newTab: false,
+  },
   {
     id: 5,
     title: "Contacto",
     path: "#contact",
+    newTab: false,
   },
 ];
 
